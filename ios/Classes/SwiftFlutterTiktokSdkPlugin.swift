d0489f359de8840809f114578c35839e4c3be535
uniwasa <dolessup@gmail.com>
2024-02-16 17:07:17 +0900

    fix: FlutterResult definition

--- ios/Classes/SwiftFlutterTiktokSdkPlugin.swift
+++ ios/Classes/SwiftFlutterTiktokSdkPlugin.swift
@@ -12,41 +12,38 @@ public class SwiftFlutterTiktokSdkPlugin: NSObject, FlutterPlugin {
     registrar.addApplicationDelegate(instance)
   }
 
+  private var result: FlutterResult?  // Does not work if defined as a method argument
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    self.result = result
     switch call.method {
     case "setup":
       result(nil)
     case "login":
-      login(call, result: result)
+      login(call)
     default:
       result(FlutterMethodNotImplemented)
       return
     }
   }
 
-  func login(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+  func login(_ call: FlutterMethodCall) {
     guard let args = call.arguments as? [String: Any] else {
-      result(FlutterError.nilArgument)
+      self.result?(FlutterError.nilArgument)
       return
     }
 
     guard let scope = args["scope"] as? String else {
-      result(FlutterError.failedArgumentField("scope", type: String.self))
+      self.result?(FlutterError.failedArgumentField("scope", type: String.self))
       return
     }
 
     guard let redirectURI = args["redirectUri"] as? String else {
-      result(FlutterError.failedArgumentField("redirectURI", type: String.self))
+      self.result?(FlutterError.failedArgumentField("redirectURI", type: String.self))
       return
     }
 
     guard let browserAuthEnabled = args["browserAuthEnabled"] as? Bool else {
-      result(FlutterError.failedArgumentField("browserAuthEnabled", type: Bool.self))
-      return
-    }
-
-    guard let rootViewController = UIApplication.shared.keyWindow?.rootViewController else {
-      result(nil)
+      self.result?(FlutterError.failedArgumentField("browserAuthEnabled", type: Bool.self))
       return
     }
 
@@ -63,9 +60,9 @@ public class SwiftFlutterTiktokSdkPlugin: NSObject, FlutterPlugin {
           "state": authRequest.state,
           "grantedPermissions": (authResponse.grantedPermissions)?.joined(separator: ","),
         ]
-        result(resultMap)
+        self.result?(resultMap)
       } else {
-        result(
+        self.result?(
           FlutterError(
             code: String(authResponse.errorCode.rawValue),
             message: authResponse.errorDescription,
